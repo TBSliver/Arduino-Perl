89b39e66a7457deb96b20d325c5e264201f462df
Tom Bloor <t.bloor@shadowcat.co.uk>
2014-09-02 22:32:29 +0100

    forgot to save a file

--- lib/File/Parse/Ino.pm
+++ lib/File/Parse/Ino.pm
@@ -3,10 +3,9 @@ package File::Parse::Ino;
 use strict;
 use warnings;
 
-use IO::All;
-use C::Tokenize qw/ $comment_re
-                    $cpp_re /;
+use File::Parse::Ino::Remove qw/ :all /;
 
+use IO::All;
 use Moo;
 use namespace::clean;
 
@@ -40,28 +39,17 @@ sub write {
   return $self->_io_output->name;
 }
 
-sub _remove_comments {
+sub collapse_braces {
   my $self = shift;
   my $string = shift;
 
-  $string =~ s/$comment_re//g;
-
-  return $string;
-}
-
-sub _remove_preprocessors {
-  my $self = shift;
-  my $string = shift;
-
-  $string =~ s/$cpp_re//g;
+  
 
   return $string;
 }
 
 sub _get_prototypes {
   my $self = shift;
-
-
 
 }
 
